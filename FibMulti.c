b78bc7f05fd865e6f075dc7eae636596e52d0255
iSlidex <45302075+iSlidex@users.noreply.github.com>
2020-01-03 21:39:32 -0400

    Cleaning stuff

--- FibMulti.c
+++ FibMulti.c
@@ -5,30 +5,6 @@
 #include <pthread.h>
     #define randnum(min,max)\
         (rand()% (int)(((max)+1))) + (min)
-/* Funcion que calculara 50000 sucesiones de fibonacci utilizando la ecuacion para
-calcular sucesion de Fibonacci y recibira un puntero a una direccion de memoria que contiene
-una variable de tipo long long int en donde se almacenara la suma de las sucesiones calculadas */
-void *Fibonacci (void *arg) {
-	double n; //Variable utilizada para el randomizado de numero de fibonacci de 0 a 19
-	long long int x = 0; //Variable en donde se iran sumando los resultados de las sucesiones
-
-	/* Ciclo que calculara 50000 sucesiones de fibonacci y las sumara */
-	for (int i=1; i<=50000; i++) {
-		//Se randomiza un numero de 0 a 19 para calcular la sucesion de fibonacci
-		srand(time(NULL));
-		//n=rand()%20;
-        n=19;
-		x += ((pow((1+sqrt(5))/2,n))-(pow((1-sqrt(5))/2,n)))/(sqrt(5)); //Se calcula la sucesion con el numero randomizado y se suma
-	}
-
-	*(long long int *)arg = x; //Se almacenara el resultado de la suma de las sucesiones fibonacci en la variable que se le pasa a la funcion
-}
-int fib(int n) 
-{ 
-    if (n <= 1) 
-        return n; 
-    return fib(n-1) + fib(n-2); 
-}
 unsigned long long int serieSum(int times){
 	if (times==0) return 0; 
     if (times==1) return 1;
