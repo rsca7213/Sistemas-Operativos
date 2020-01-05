6165376fb0f4d37daad8d58e1dc80b634785d5e2
rsca7213 <rsca4321@gmail.com>
2020-01-05 19:50:27 -0400

    retocar el codigo de fibonacci y comentarlo

--- FibMulti.c
+++ FibMulti.c
@@ -3,8 +3,16 @@
 #include <math.h>
 #include <time.h>
 #include <pthread.h>
-    #define randnum(min,max)\
-        (rand()% (int)(((max)+1))) + (min)
+
+/*Se define randnum(), la cual generara un numero aleatorio en el rango 
+establecido por los parametros min max */
+#define randnum(min,max)\
+    (rand()% (int)(((max)+1))) + (min)
+
+/*Función utilizada para calcular la secuencia de fibonacci, en esta
+funcion se recibe un numero entero "times", el cual se utilizara para
+calcular la sucesion de fibonacci utilizando el metodo del ciclo que ira
+sumando los numeros anteriores para calcular la sucesion*/
 unsigned long long int serieSum(int times){
 	if (times==0) return 0; 
     if (times==1) return 1;
@@ -18,6 +26,11 @@ unsigned long long int serieSum(int times){
     	}
     return count;
 }
+
+/*Funcion que generara numeros aleatorios de 0 a 19 y llamara a la funcion serieSum para que
+calcule la sucesion de fibonacci con el numero aleatorio generado (esto lo hara 50000 veces), los resultados los ira
+almacenando en la variable suma, y al finalizar se pasara a la variable parametro de la funcion "arg", la suma total
+de los resultados */
 void * roll(void * arg){
     unsigned long long int suma = 0;
     int times = 50000; 
@@ -27,23 +40,24 @@ void * roll(void * arg){
     *(long long int *)arg  = suma;
 }
 
-
+//inicio de funcion principal
 int main() {
+	srand(time(0)); //se genera una semilla para el randomizador de numeros, basado en el tiempo local de la maquina
 	printf("\tProcesando sucesiones de fibonacci...\n");
-    srand(time(0));
 	clock_t start_t, end_t; 
 	start_t = clock(); //Inicio de contador de tiempo de procesamiento del programa
 	long long int x=0; //Variable en la que se almacenara la suma de todas las sucesiones de fibonacci calculadas
 	long long int n[20]; //Variables que utilizara cada hilo para evitar condiciones de carrera
 	for (int i=0; i<=19; i++) {n[i]=0;} //Se inicializan las variables que utilizaran los hilos en 0
     /* Se crean 20 hilos que trabajaran en paralelo y calcularan 50000 sucesiones cada uno de ellos
-       utilizando la funcion Fibonacci*/
+       utilizando la funcion roll(void *arg)*/
 	pthread_t hilo[20];
 	for (int i=0; i<=19; i++) {
+		//Se crea un hilo que trabajara con la funcion roll y se le pasa n[i] como parametro
 		pthread_create(&hilo[i],NULL,&roll,&n[i]);	
 	}
 
-	/* Se espera a que terminen los 20 hilos y se van sumando los resultados obtenidos en la variable de resultados x */
+	/* Se espera a que terminen los 20 hilos y se van sumando los resultados obtenidos en la variable de resultados "x" */
 	for (int i=0; i<=19; i++) {
 		pthread_join(hilo[i],NULL);
 		x += n[i];
@@ -53,6 +67,6 @@ int main() {
 	printf("\n\t\tInformacion Final\n");
 	printf("\tNumero Calculado ---> %lli \n", x);
 	printf("\tTiempo transcurrido en segundos ---> %.2f \n", (double)(end_t-start_t)/CLOCKS_PER_SEC);
-	printf("\tSucesiones calculadas ---> 50000 por cada hilo (1000000 total)\n");
+	printf("\tSucesiones calculadas ---> 50000 por cada hilo (20 hilos) (1000000 total)\n");
     return 0;
 }
--- FibSecuencial.c
+++ FibSecuencial.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-    #define randnum(min,max)\
-        (rand()% (int)(((max)+1))) + (min)
-/* Funcion que utiliza la ecuacion para hallar la sucesion de fibonacci y recibe un parametro n que seria
-   el numero de fibonacci a calcular*/
 
+/*Se define randnum(), la cual generara un numero aleatorio en el rango 
+establecido por los parametros min max */
+#define randnum(min,max)\
+    (rand()% (int)(((max)+1))) + (min)
+
+/*Función utilizada para calcular la secuencia de fibonacci, en esta
+funcion se generara un numero aleatorio de 0 a 19 que se utilizara para
+calcular la sucesion de fibonacci utilizando el metodo del ciclo que ira
+sumando los numeros anteriores para calcular la sucesion*/
 unsigned long long int serieSum(){    
     int times =randnum(0,19);	
     if (times==0) return 0; 
@@ -21,16 +26,16 @@ unsigned long long int serieSum(){
     	}
     return count;
 }
+
+//inicio de funcion principal
 void main () {
-    srand(time(0));
+    srand(time(0)); //Se genera una semilla para el randomizador de numeros basandose en el tiempo actual del reloj local
     printf("\tProcesando sucesiones de fibonacci...\n");
     clock_t start_t = clock(); //Se inicia el contador de tiempo de procesamiento del programa
-	double i; //Variable en la que se almacenara un valor de 0 a 19 randomizado para calcular sucesion de fibonacci
 	long long int res=0; //Variable en la que se almacenara la suma de todas las sucesiones calculadas
-	/* Ciclo que calculara un millon de sucesiones de fibonacci y las sumara */
+	/* Ciclo que calculara un millon de sucesiones de fibonacci utilzando la funcion serieSum() y las sumara */
 	for (int j=1; j<=1000000; j++) {
-		//Se randomiza un numero de 0 a 19		
-		res+=serieSum(); //Se calcula la sucesion de fibonacci con el numero randomizado y se le suma a la variable de resultado
+		res+=serieSum(); //Se calcula una sucesion de fibonacci y se le suma a la variable de resultado "res"
 	}
 
 	clock_t end_t = clock(); //Se finaliza el contador de tiempo de procesamiento del programa
