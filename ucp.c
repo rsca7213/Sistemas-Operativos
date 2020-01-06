28a0ec07d4f14acf4da66cf1a8f944d78352270a
rsca7213 <rsca4321@gmail.com>
2020-01-06 13:45:01 -0400

    limpiar el codigo de ucp y comentarlo

--- ucp.c
+++ ucp.c
@@ -13,88 +13,104 @@
 void error(char *, char *);
 int copyFiles(int buff,char *src, char *dest);
 
-int mode_isReg(struct stat info);
- 
+/*inicio de funcion principal, tiene 2 argumentos:
+  ac = numero de argumentos que se pasaron en consola
+  av = arreglo de los argumentos que se pasaron en consola */
 int main(int ac, char *av[])
 {
-  int buff = atoi(av[1]);
+    int buff = atoi(av[1]); //Se transforma el string de buffer a un entero y se almacena en la variable buff
 
-  fprintf(stderr, "Buffer: %i\nArchivo Origen: %s\nArchivo destino: %s\n",buff,av[2],av[3]);
-/* Verificacion de buffer*/
-  if (buff>0 && buff<=16384){
-/* Verificacion de argumentos */
-  if(ac != 4)
-  {
-    fprintf(stderr, "usage: %s source destination\n", *av);
-    exit(1);
-  } 
+    //Se imprime en pantalla los datos recibidos por la consola
+    fprintf(stderr, "Buffer: %i\nArchivo Origen: %s\nArchivo destino: %s\n",buff,av[2],av[3]);
+    /* Verificacion de buffer */
+    if (buff>0 && buff<=16384){
+        /* Verificacion de argumentos */
+        if(ac != 4)
+        {
+            fprintf(stderr, "usage: %s source destination\n", *av);
+            exit(1);
+        } 
  
-   char *src = av[2];
-   char *dest = av[3]; 
+        char *src = av[2]; //Se almacena la ruta del archivo origen
+        char *dest = av[3]; //Se almacena la ruta del archivo destino
  
-   if( src[0] != '/' && dest[0] != '/' )//ucp buffer file1.txt file2.txt
-   {
-       copyFiles(buff ,src ,dest );
-   }
-   else if( src[0] != '/' && dest[0] == '/' )//ucp buffer file1.txt /dir 
-   {
-      int i;
-      for(i=1; i<=strlen(dest); i++)
-      {
-          dest[(i-1)] = dest[i];
-      }
-      strcat(dest, "/");
-      strcat(dest, src);
-      copyFiles(buff,src, dest);
-  }
-  else
-  {
-      fprintf(stderr, "Modo de uso: ucp Buffer source destination\n");
-      exit(1);
-  }
-  }else {
-    fprintf(stderr,"El tamano del buffer : es %i  pero este debe estar entre 1 y 16384",buff);
-    exit(1);
-  }  
+        if( src[0] != '/' && dest[0] != '/' )//ucp buffer file1.txt file2.txt
+        {
+            copyFiles(buff ,src ,dest ); //Se llama a la funcion copyFiles con los parametros pasados por consola
+        }
+        else if( src[0] != '/' && dest[0] == '/' )//ucp buffer file1.txt /dir 
+        {
+            /* Se realizan los cambios necesarios en la ruta del archivo destino 
+            "se elimina el / inicial y se coloca un / al final, luego se concatena el nombre
+            del archivo origen"
+            */
+            int i; 
+            for(i=1; i<=strlen(dest); i++)
+            {
+                dest[(i-1)] = dest[i];
+            }
+            strcat(dest, "/");
+            strcat(dest, src);
+            copyFiles(buff,src, dest); //Se llama a la funcion copyFiles con el buffer y las rutas a utilizar
+        }
+        else //Si hay un error en el archivo origen
+        {
+            fprintf(stderr, "Modo de uso: ucp Buffer source destination\n");
+            exit(1);
+        }
+    }
+    else //Si hay un error en el tamaño del buffer
+    {
+        fprintf(stderr,"El tamano del buffer : es %i  pero este debe estar entre 1 y 16384",buff);
+        exit(1);
+    }  
 }
 
- 
+/* Funcion que realiza la copia de los archivos y
+recibe como parametros el tamaño del buffer y 
+las rutas de origen y destino utilizadas */
 int copyFiles(int BUFFERSIZE,char *source, char *destination)
 {
-  int in_fd, out_fd, n_chars;
-  char buf[BUFFERSIZE];
+    int in_fd, out_fd, n_chars; //Declaracion de los file
+    /* Declaracion de arreglo a ser utilizado como buffer con el tamaño 
+    especificado por el parametro BUFFERSIZE */
+    char buf[BUFFERSIZE]; 
  
-  /* open files */
-  if( (in_fd=open(source, O_RDONLY)) == -1 )
-  {
-    error("No se puede abrir ", source);
-  }
-  if( (out_fd=creat(destination, COPYMORE)) == -1 )
-  {
-    error("No se puede leer ", destination);
-  }
-  /* copy files */
-  while( (n_chars = read(in_fd, buf, BUFFERSIZE)) > 0 )
-  {
-    if( write(out_fd, buf, n_chars) != n_chars )
+    /* Se realiza la apertura de los archivos, si se da un error se
+    imprime por pantalla */
+    if( (in_fd=open(source, O_RDONLY)) == -1 )
     {
-      error("Error de escritura en: ", destination);
+        error("No se puede abrir ", source);
     }
- 
- 
-    if( n_chars == -1 )
+    if( (out_fd=creat(destination, COPYMORE)) == -1 )
     {
-      error("Error de lectura de: ", source);
+        error("No se puede leer ", destination);
     }
-  } 
-    /* close files */
+    /* Se realiza la copia de los archivos */
+    while( (n_chars = read(in_fd, buf, BUFFERSIZE)) > 0 )
+    {
+        if( write(out_fd, buf, n_chars) != n_chars )
+        {
+            error("Error de escritura en: ", destination);
+        }
+        if( n_chars == -1 )
+        {
+            error("Error de lectura de: ", source);
+        }
+    } 
+    /* Se cierran los archivos al terminar de realizar la copia, si se da un error
+    se imprime por pantalla */
     if( close(in_fd) == -1 || close(out_fd) == -1 )
     {
       error("Error al cerrar archivo", "");
     }
+
     return 1;
 }
+// Fin de funcion para la copia de archivos
  
+/* Funcion que imprime por pantalla los errores que han sucedido durante la ejecucion
+del programa UCP */
 void error(char *s1, char *s2)
 {
     fprintf(stderr, "Error: %s ", s1);
